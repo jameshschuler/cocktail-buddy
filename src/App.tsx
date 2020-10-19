5a174e61eccd184384c6ac4da1dcfa7031911813
James Schuler <jamesschuler@jamess-mbp.lan>
2020-10-18 22:03:42 -0400

    firebase setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import 'milligram';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -5,6 +6,17 @@ import Content from './components/Content';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './styles/app.scss';
+
+export const app = firebase.initializeApp({
+	apiKey: 'AIzaSyCpeTDJJcwgRmPstxWv092NPSH2lUBpHYA',
+	authDomain: 'cocktail-buddy-ebdcc.firebaseapp.com',
+	databaseURL: 'https://cocktail-buddy-ebdcc.firebaseio.com',
+	projectId: 'cocktail-buddy-ebdcc',
+	storageBucket: 'cocktail-buddy-ebdcc.appspot.com',
+	messagingSenderId: '112177370180',
+	appId: '1:112177370180:web:b3cc2df85e3ed245701811',
+	measurementId: 'G-BK271LJHN6',
+});
 
 function App() {
 	return (
